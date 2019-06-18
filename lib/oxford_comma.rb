b61470e7ada93db37a1a6d4fea68e119f71467ae
aminamos <26092352+aminamos@users.noreply.github.com>
2019-06-18 13:42:09 -0700

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+     if array.length == 1
+        array[0]
+     elsif array.length == 2
+        array.join(" and ")
+     elsif array.length > 2
+        # array.join(", ") << "and #{array[-1]}"
+        array.reverse.drop(1).reverse.join(", ") << ", and #{array[-1]}" # thanks, odd stackoverflow solution...
+     end
 end
